7ff6b46e913a658e896008d8c188c3b172d915a0
Akron <nils@diewald-online.de>
2018-08-23 17:15:43 +0200

    Put caching in toFilter() phase and made external test suite internal
    Change-Id: Id118ae1f67050e8d0362ae3338be7e65746e2e64

--- src/test/java/de/ids_mannheim/korap/collection/TestCollectionBuilder.java
+++ src/test/java/de/ids_mannheim/korap/collection/TestCollectionBuilder.java
@@ -86,6 +86,14 @@ public class TestCollectionBuilder {
 
 
     @Test
+    public void builderReference () throws IOException {
+        CollectionBuilder kc = new CollectionBuilder();
+        assertEquals("referTo(ndiewald/myCorpus)",
+                kc.referTo("ndiewald/myCorpus").toString());
+    };
+	
+
+    @Test
     public void builderTill () throws IOException {
         CollectionBuilder kc = new CollectionBuilder();
         assertEquals("pubDate:[0 TO 20059999]",
