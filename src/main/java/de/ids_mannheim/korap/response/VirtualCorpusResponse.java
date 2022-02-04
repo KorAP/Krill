a4d543dd223dc082b7a3ea2ef713f0ecf66f5925
margaretha <margaretha@ids-mannheim.de>
2022-02-04 09:27:35 +0100

    Replaced array nodes of field values with a simple string.
    Change-Id: Icdccc3440d47abf1ce2a91fb39840e6344fa5bd5

--- src/main/java/de/ids_mannheim/korap/response/VirtualCorpusResponse.java
+++ src/main/java/de/ids_mannheim/korap/response/VirtualCorpusResponse.java
@@ -1,26 +1,34 @@
 package de.ids_mannheim.korap.response;
 
 import java.util.List;
+import static de.ids_mannheim.korap.util.KrillString.quote;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.RawValue;
 
 public class VirtualCorpusResponse extends Response {
 
     public JsonNode createKoralQueryForField (String fieldName,
             List<String> fieldValues) {
 
-        ArrayNode arrayNode = mapper.createArrayNode();
-        for (String v : fieldValues) {
-            arrayNode.add(v);
+        StringBuffer sb = new StringBuffer("[");
+        int size = fieldValues.size();
+        for (int i=0; i < size; i++) {
+            if (fieldValues.get(i).length() == 0)
+                continue;
+            sb.append(quote(fieldValues.get(i)));
+            if (i < size-1) {
+                sb.append(",");
+            }
         }
+        sb.append("]");
 
         ObjectNode collectionNode = mapper.createObjectNode();
         collectionNode.put("@type", "koral:doc");
         collectionNode.put("key", fieldName);
         collectionNode.put("type", "type:string");
-        collectionNode.set("value", arrayNode);
+        collectionNode.putRawValue("value", new RawValue(sb.toString()));
 
         ObjectNode root = mapper.createObjectNode();
         root.put("@context", KORAL_VERSION);
