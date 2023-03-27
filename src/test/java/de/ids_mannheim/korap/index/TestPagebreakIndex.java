8806037be57f9b4ce50bbbde2d89d4445420bdae
margaretha <margaretha@ids-mannheim.de>
2023-03-27 16:20:08 +0200

    Test pagebreak doc lower than match doc number.
    Change-Id: I6255b44c523c7fea1656c78a1aa18db1febfc4c3

--- src/test/java/de/ids_mannheim/korap/index/TestPagebreakIndex.java
+++ src/test/java/de/ids_mannheim/korap/index/TestPagebreakIndex.java
@@ -1,33 +1,18 @@
 package de.ids_mannheim.korap.index;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.spans.SpanOrQuery;
 import org.apache.lucene.search.spans.SpanQuery;
 import org.apache.lucene.search.spans.SpanTermQuery;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import de.ids_mannheim.korap.KrillCollection;
-import de.ids_mannheim.korap.Krill;
 import de.ids_mannheim.korap.KrillIndex;
-import de.ids_mannheim.korap.query.QueryBuilder;
-import de.ids_mannheim.korap.query.SpanClassQuery;
-import de.ids_mannheim.korap.query.SpanElementQuery;
-import de.ids_mannheim.korap.query.SpanFocusQuery;
-import de.ids_mannheim.korap.query.SpanNextQuery;
-import de.ids_mannheim.korap.query.SpanWithinQuery;
-import de.ids_mannheim.korap.query.QueryBuilder;
-import de.ids_mannheim.korap.query.wrap.SpanQueryWrapper;
-import de.ids_mannheim.korap.response.Match;
 import de.ids_mannheim.korap.response.Result;
-import de.ids_mannheim.korap.response.SearchContext;
 
 /*
  * Retrieve pagebreak annotations
@@ -35,25 +20,56 @@ import de.ids_mannheim.korap.response.SearchContext;
 
 @RunWith(JUnit4.class)
 public class TestPagebreakIndex {
+    
+    private FieldDocument createFieldDoc0 () {
+        // abcde
+           FieldDocument fd = new FieldDocument();
+           fd.addTV("tokens", "abcde",
+                    "[(0-1)s:a|i:a|_0$<i>0<i>1|-:t$<i>5]" +
+                    "[(1-2)s:b|i:b|_1$<i>1<i>2]" +
+                    "[(2-3)s:c|i:c|_2$<i>2<i>3]" +
+                    "[(3-4)s:a|i:d|_3$<i>3<i>4]" +
+                    "[(4-5)s:b|i:e|_4$<i>4<i>5]"
+                    );
+           return fd;
+       }
+    
+    private FieldDocument createFieldDoc1 () {
+     // abcabcabac
+        FieldDocument fd = new FieldDocument();
+        fd.addTV("tokens", "abcabcabac",
+                 "[(0-1)s:a|i:a|_0$<i>0<i>1|-:t$<i>10|~:base/s:pb$<i>528<i>0]" +
+                 "[(1-2)s:b|i:b|_1$<i>1<i>2]" +
+                 "[(2-3)s:c|i:c|_2$<i>2<i>3]" +
+                 "[(3-4)s:a|i:a|_3$<i>3<i>4]" +
+                 "[(4-5)s:b|i:b|_4$<i>4<i>5]" +
+                 "[(5-6)s:c|i:c|_5$<i>5<i>6|~:base/s:pb$<i>529<i>5]" +
+                 "[(6-7)s:a|i:a|_6$<i>6<i>7]" +
+                 "[(7-8)s:b|i:b|_7$<i>7<i>8]" +
+                 "[(8-9)s:a|i:a|_8$<i>8<i>9|~:base/s:pb$<i>530<i>8]" +
+                 "[(9-10)s:c|i:c|_9$<i>9<i>10]");
+        return fd;
+    }
+    
+    @Test
+    public void testPageBreakDocLowerThanLocalDocId () throws IOException {
+        KrillIndex ki = new KrillIndex();
+        ki.addDoc(createFieldDoc0());
+        ki.addDoc(createFieldDoc1());
+        ki.commit();
+        
+        SpanTermQuery sq = new SpanTermQuery(new Term("tokens", "s:c"));
+        Result kr = ki.search(sq, (short) 10);
+        assertEquals(4, kr.getMatches().size());
+
+    }
 
     @Test
     public void indexExample1 () throws Exception {
 		KrillIndex ki = new KrillIndex();
 
 		// abcabcabac
-		FieldDocument fd = new FieldDocument();
-		fd.addTV("tokens", "abcabcabac",
-				 "[(0-1)s:a|i:a|_0$<i>0<i>1|-:t$<i>10|~:base/s:pb$<i>528<i>0]" +
-				 "[(1-2)s:b|i:b|_1$<i>1<i>2]" +
-				 "[(2-3)s:c|i:c|_2$<i>2<i>3]" +
-				 "[(3-4)s:a|i:a|_3$<i>3<i>4]" +
-				 "[(4-5)s:b|i:b|_4$<i>4<i>5]" +
-				 "[(5-6)s:c|i:c|_5$<i>5<i>6|~:base/s:pb$<i>529<i>5]" +
-				 "[(6-7)s:a|i:a|_6$<i>6<i>7]" +
-				 "[(7-8)s:b|i:b|_7$<i>7<i>8]" +
-				 "[(8-9)s:a|i:a|_8$<i>8<i>9|~:base/s:pb$<i>530<i>8]" +
-				 "[(9-10)s:c|i:c|_9$<i>9<i>10]");
-        ki.addDoc(fd);
+        ki.addDoc(createFieldDoc1());
         ki.commit();
 
 		SpanQuery sq;
