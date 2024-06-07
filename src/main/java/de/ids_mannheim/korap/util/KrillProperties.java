e7d0dd247a917cfbacf5d825654e32ee3e10f3ad
margaretha <margaretha@ids-mannheim.de>
2024-06-07 12:19:51 +0200

    Set the default value for named-vc path (fixes #146)
    Change-Id: I77d0ac01bddba2c1649e88385ed475e46d56ea47

--- src/main/java/de/ids_mannheim/korap/util/KrillProperties.java
+++ src/main/java/de/ids_mannheim/korap/util/KrillProperties.java
@@ -23,6 +23,9 @@ public class KrillProperties {
     public static int maxTokenContextSize = 60;
     public static int maxCharContextSize = 500;
     
+    public static String namedVCPath = "";
+    public static boolean isTest = false;
+    
     // Logger
     private final static Logger log = LoggerFactory
             .getLogger(KrillProperties.class);
@@ -74,7 +77,7 @@ public class KrillProperties {
         return prop;
     };
 
-    private static void updateConfigurations (Properties  prop) {
+    public static void updateConfigurations (Properties  prop) {
         String maxTokenMatchSize = prop.getProperty("krill.match.max.token");
         String maxTokenContextSize = prop.getProperty("krill.context.max.token");
 
@@ -92,6 +95,11 @@ public class KrillProperties {
             log.error("A Krill property expects numerical values: "
                     + e.getMessage());
         };
+        
+        String p = prop.getProperty("krill.test", "false");
+        isTest = Boolean.parseBoolean(p);
+        
+        namedVCPath = prop.getProperty("krill.namedVC", "");
     }
     
 
