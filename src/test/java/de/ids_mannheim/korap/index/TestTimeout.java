d7c567d2aa2759573848e4fbc484fd644e7c8646
margaretha <margaretha@ids-mannheim.de>
2024-11-15 11:53:41 +0100

    Test timeout
    Change-Id: Ib19d602337b2c1870f082dc36739c1a7431b96f8

--- src/test/java/de/ids_mannheim/korap/index/TestTimeout.java
+++ src/test/java/de/ids_mannheim/korap/index/TestTimeout.java
@@ -9,13 +9,16 @@ import org.apache.lucene.search.spans.SpanQuery;
 import org.apache.lucene.search.spans.SpanTermQuery;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import de.ids_mannheim.korap.Krill;
 import de.ids_mannheim.korap.KrillIndex;
 import de.ids_mannheim.korap.KrillMeta;
+import de.ids_mannheim.korap.TestSimple;
 import de.ids_mannheim.korap.response.Result;
 import de.ids_mannheim.korap.util.StatusCodes;
 
-public class TestTimeout {
+public class TestTimeout extends TestSimple {
 
     @Test
     public void testMultipleWarningBug () throws IOException {
@@ -51,5 +54,22 @@ public class TestTimeout {
                 kr.getWarning(0).getCode());
     }
 
+    @Test
+    public void testMaxTimeout () throws IOException {
+        KrillIndex ki = new KrillIndex();
+        ki.addDoc(getClass().getResourceAsStream("/wiki/00001.json.gz"), true);
+        ki.commit();
 
+        String json = getJsonString(getClass()
+                .getResource("/queries/metas/timeout.jsonld").getFile());
+        Krill ks = new Krill(json);
+        Result kr = ks.apply(ki);
+        KrillMeta meta = kr.getMeta();
+        
+        //EM: 120000 seems to be the default
+        assertEquals("120000", meta.getTimeOut());
+        
+        JsonNode jsonNode = kr.toJsonNode();
+        assertEquals("120000", jsonNode.at("/meta/timeout").asInt());        
+    }
 }
