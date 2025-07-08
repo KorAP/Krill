645d3d3fde9077bdb927055449bf5a535f46d16d
Akron <nils@diewald-online.de>
2025-07-08 15:52:45 +0200

    Add leaf cache
    Change-Id: Ib630e0327cf64c246c3648361e5a6844ea30cf3a

--- src/main/java/de/ids_mannheim/korap/KrillIndex.java
+++ src/main/java/de/ids_mannheim/korap/KrillIndex.java
@@ -57,6 +57,8 @@ import org.apache.lucene.util.automaton.RegExp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.benmanes.caffeine.cache.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 // Krill classes
@@ -179,6 +181,17 @@ public final class KrillIndex implements IndexInfo {
     private HashMap termContexts;
     private ObjectMapper mapper = new ObjectMapper();
 
+    // Prelim CacheKey
+    record PrelimCacheKey(int queryHash, int collHash, short itemsPerRessource) {}
+
+    // CacheKey
+    record SearchCacheKey(PrelimCacheKey prelimck, String atomicHash) {}
+
+    // CacheValue
+    record SearchCacheValue(int matchCount, int matchDocCount) {}
+
+    Cache<SearchCacheKey, SearchCacheValue> searchCache;
+    
     // private ByteBuffer bbTerm;
 
     // Some initializations ...
@@ -193,8 +206,11 @@ public final class KrillIndex implements IndexInfo {
 
         // Check for auto commit value
         String autoCommitStr = null;
+        String cacheSizeStr = null;
+        int cacheSize = (64 * 1024 * 1024); // 64 MB 
         if (prop != null) {
             autoCommitStr = prop.getProperty("krill.index.commit.auto");
+            cacheSizeStr = prop.getProperty("krill.cache.size");
         }
         
         if (autoCommitStr != null) {
@@ -206,6 +222,20 @@ public final class KrillIndex implements IndexInfo {
                         "krill.index.commit.auto expected to be a numerical value");
             };
         };
+
+        if (cacheSizeStr != null) {
+            try {
+                int retVal = Integer.parseInt(cacheSizeStr);
+                cacheSize = retVal;
+            } catch (NumberFormatException e) {
+                log.warn("krill.cache.size expected to be a numerical value");
+            }
+        };
+
+        searchCache = Caffeine.newBuilder()
+            .maximumWeight(cacheSize)
+            .weigher((SearchCacheKey key, SearchCacheValue value) -> 80) // estimate per-entry size
+            .build();
     };
 
 
@@ -1459,9 +1489,11 @@ public final class KrillIndex implements IndexInfo {
         tthread.start();
         final long timeout = meta.getTimeOut();
         boolean isTimeout = false;
-
+       
         // See: http://www.ibm.com/developerworks/java/library/j-benchmark1/index.html
         long t1 = System.nanoTime();
+
+        int fromCache = 0;
 
         try {
             // Rewrite query (for regex and wildcard queries)
@@ -1478,6 +1510,14 @@ public final class KrillIndex implements IndexInfo {
 			if (DEBUG)
 				log.trace("Rewritten query is {}", query.toString());
 
+            int collHash = 0;
+            Filter collf = collection.toFilter();
+            if (collf != null) {
+                collHash = collf.hashCode();
+            };
+
+            PrelimCacheKey prelim = new PrelimCacheKey(query.hashCode(), collHash, itemsPerResource);
+            
             // Todo: run this in a separated thread
             for (LeafReaderContext atomic : this.reader().leaves()) {
 
@@ -1485,15 +1525,55 @@ public final class KrillIndex implements IndexInfo {
 
                 if (isTimeout)
                     break;
+
+                SearchCacheKey finalCacheKey = new SearchCacheKey(prelim, atomic.reader().getCombinedCoreAndDeletesKey().toString());
+                SearchCacheValue foundCache = searchCache.getIfPresent(finalCacheKey);
                 
+                if (foundCache != null) {
+                    if (DEBUG) {
+                        log.trace(
+                            "Found cache for Query: {}, Collection: {}, itemsPerRessource: {}, Reader: {}",
+                            query.hashCode(), collHash, itemsPerResource, atomic.reader().getCombinedCoreAndDeletesKey().toString()
+                            );
+                    };
+
+                    if (startIndex > (i + foundCache.matchCount)) {
+                        fromCache += foundCache.matchCount;
+                        i += foundCache.matchCount;
+                        j += foundCache.matchDocCount;
+                        continue;
+                    };
+                } else if (DEBUG) {
+                    log.trace(
+                        "Found no cache for Query: {}, Collection: {}, itemsPerRessource: {}, Reader: {}",
+                        query.hashCode(), collHash, itemsPerResource, atomic.reader().getCombinedCoreAndDeletesKey().toString()
+                        );
+                } 
+
                 /*
                  * Todo: There may be a way to know early if the bitset is emty
                  * by using LongBitSet - but this may not be as fast as I think.
                  */
                 final FixedBitSet bitset = collection.bits(atomic);
 
-				if (bitset.nextSetBit(0) == DocIdSetIterator.NO_MORE_DOCS)
+				if (bitset.nextSetBit(0) == DocIdSetIterator.NO_MORE_DOCS) {
+                    if (foundCache == null)
+                        searchCache.put(
+                            finalCacheKey,
+                            new SearchCacheValue(0, 0)
+                            );
+                    
+                    if (DEBUG) {
+                        log.trace(
+                            "Store cache (1) for Query: {}, Collection: {}, itemsPerRessource: {}, Reader: {}, store:0/0",
+                            query.hashCode(),
+                            collHash,
+                            itemsPerResource,
+                            atomic.reader().getCombinedCoreAndDeletesKey().toString()
+                            );
+                    };
 					continue;
+                };
 
                 final PositionsToOffset pto = snippets ? new PositionsToOffset(atomic, field) : null;
 				
@@ -1504,6 +1584,9 @@ public final class KrillIndex implements IndexInfo {
                 final IndexReader lreader = atomic.reader();
                 int localDocID, docID;
 
+                int li = i;
+                int lj = j;
+                
                 // TODO: Get document information from Cache! Fieldcache?
                 for (; i < hits; i++) {
 
@@ -1511,9 +1594,28 @@ public final class KrillIndex implements IndexInfo {
                         log.trace("Match Nr {}/{}", i, count);
                    
                     // There are no more spans to find
-                    if (!spans.next())
-                        break;
+                    if (!spans.next()) {
+                        if (foundCache == null)
+                            foundCache = new SearchCacheValue(i - li, j - lj);
+                            searchCache.put(
+                                finalCacheKey,
+                                foundCache
+                                );
 
+                        if (DEBUG) {
+                            log.trace(
+                                "Store cache (2) for Query: {}, Collection: {}, itemsPerRessource: {}, Reader: {}, store:{}/{}",
+                                query.hashCode(),
+                                collHash,
+                                itemsPerResource,
+                                atomic.reader().getCombinedCoreAndDeletesKey().toString(),
+                                i - li,
+                                j - lj
+                                );
+                        };
+                        break;
+                    };
+                    
                     // Increment resource counter
                     itemsPerResourceCounter++;
                     
@@ -1680,6 +1782,28 @@ public final class KrillIndex implements IndexInfo {
                     oldLocalDocID = localDocID;
                     i++;
                 };
+
+                if (!isTimeout && !cutoff) {
+                    if (foundCache == null) {
+                        searchCache.put(
+                            finalCacheKey,
+                            new SearchCacheValue(i - li, j -lj)
+                            );
+                    
+                        if (DEBUG) {
+                            log.trace(
+                                "Store cache (3) for Query: {}, Collection: {}, itemsPerRessource: {}, Reader: {}, store:{}/{}",
+                                query.hashCode(),
+                                collHash,
+                                itemsPerResource,
+                                atomic.reader().getCombinedCoreAndDeletesKey().toString(),
+                                i - li,
+                                j - lj
+                                );
+                        };
+                    };
+                };
+                
                 atomicMatches.clear();
             };
 
@@ -1711,6 +1835,9 @@ public final class KrillIndex implements IndexInfo {
             log.error(e.getMessage());
             e.printStackTrace();
         }
+
+        if (fromCache > 0)
+            kr.addMessage(0, "Some results were cached", String.valueOf(fromCache));
 
         // Stop timer thread
         tthread.stopTimer();
